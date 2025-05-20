b875c73f023b34574f9ea9fe637219ee333a1f20
dikac <43611660+dikac@users.noreply.github.com>
2025-05-20 16:46:30 +0700

    Update pkg/error_handler/generic.go
    Co-authored-by: Muhammad Syahidin <muhammadsyahidien@gmail.com>

--- pkg/error_handler/generic.go
+++ pkg/error_handler/generic.go
@@ -16,4 +16,22 @@ func (g *Generic) Handle(err error, ctx echo.Context) error {
 	ctx.Response().Status = 500
 	// addition code for body or header here
 	return nil
+package errorhandler
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+type GenericHandler struct {
+}
+
+func NewGeneric() *GenericHandler {
+	return &GenericHandler{}
+}
+
+func (g *Generic) Handle(err error, ctx echo.Context) error {
+
+	ctx.Response().Status = 500
+	// addition code for body or header here
+	return nil
 }
