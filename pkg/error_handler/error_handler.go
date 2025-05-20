a3a346bec0d45fd876e24f5c583fe753a16f5abe
dikac <43611660+dikac@users.noreply.github.com>
2025-05-20 16:45:46 +0700

    Update pkg/error_handler/error_handler.go
    Co-authored-by: Muhammad Syahidin <muhammadsyahidien@gmail.com>

--- pkg/error_handler/error_handler.go
+++ pkg/error_handler/error_handler.go
@@ -4,4 +4,32 @@ import "github.com/labstack/echo/v4"
 
 type ErrorHandler interface {
 	Handle(err error, ctx echo.Context) error
+package errorhandler
+
+import "github.com/labstack/echo/v4"
+
+
+type ErrorHandler interface {
+	Handle(err error, ctx echo.Context) error
+}
+
+type Chain []ErrorHandler
+
+func NewChain(handlers ...ErrorHandler) Chain {
+	return handlers
+}
+
+func (c Chain) Handle(err error, ctx echo.Context) error {
+	for _, h := range c {
+		if h.Handle(err, ctx) == nil {
+			return nil
+		}
+	}
+	return err
+}
+
+func (c Chain) EchoHandler(err error, ctx echo.Context) {
+	if remaining := c.Handle(err, ctx); remaining != nil {
+		echo.DefaultHTTPErrorHandler(remaining, ctx)
+	}
 }
