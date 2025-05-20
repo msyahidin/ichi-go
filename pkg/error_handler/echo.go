fea52189d5ae0132b06d5e1a00587f3b2190669a
dikac <43611660+dikac@users.noreply.github.com>
2025-05-20 16:46:03 +0700

    Update pkg/error_handler/echo.go
    Co-authored-by: Muhammad Syahidin <muhammadsyahidien@gmail.com>

--- pkg/error_handler/echo.go
+++ pkg/error_handler/echo.go
@@ -23,4 +23,13 @@ func (h *Echo) Handle(err error, ctx echo.Context) error {
 	}
 
 	return err
+func (h *EchoHandler) Handle(err error, c echo.Context) error {
+    var httpErr *echo.HTTPError
+    if errors.As(err, &httpErr) {
+    
+       // addition code for body or header here
+
+	return nil
+    }
+    return err
 }
